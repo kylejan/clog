c3b9da5fc7b94f5a50f7da53f41bcea13612db98
kylejan <765108938@qq.com>
2016-01-09 22:04:13 +0800

    update

--- main.cpp
+++ main.cpp
@@ -8,23 +8,23 @@ int main()
     get_clog()->init();
 
     std::thread* thread_1 = new std::thread([]{
-        int num = 1000000;
+        int num = 500000;
         while (num--)
         {
             get_clog()->info("({:s}, {:d})", "thread_1", num);
         }
     });
 
-    // std::thread* thread_2 = new std::thread([]{
-    //     int num = 100000;
-    //     while (num--)
-    //     {
-    //         get_clog()->warn("({:s}, {:d})", "thread_2", num);
-    //     }
-    // });
+    std::thread* thread_2 = new std::thread([]{
+        int num = 500000;
+        while (num--)
+        {
+            get_clog()->warn("({:s}, {:d})", "thread_2", num);
+        }
+    });
 
     thread_1->join();
-    // thread_2->join();
+    thread_2->join();
 
     return 0;
 }
